b0ce28179063ac388f973338f128dab2801d354d
e <1792314541@qq.com>
2019-11-18 20:42:02 +0800

    2019-11-18 20:41 喻浩
    初始化项目的基本配置，以及路径内文件的编写

---
+++ src/main/java/Login.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 喻浩
+ * @create 2019-11-18-20:31
+ */
+@WebServlet("/")
+public class Login extends HttpServlet {
+    
+    
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        
+    }
+}
